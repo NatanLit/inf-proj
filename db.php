20d299259085979718ff60491cd65436e3d4d982
NatanLit <litvinovnatan@gmail.com>
2026-05-18 19:56:26 +0500

    fix: use Railway env variables for DB connection

--- db.php
+++ db.php
@@ -1,10 +1,11 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "stabilia";
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+$db   = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $user, $pass, $db, (int)$port);
 
 if (!$conn) {
     die("Ошибка подключения: " . mysqli_connect_error());
